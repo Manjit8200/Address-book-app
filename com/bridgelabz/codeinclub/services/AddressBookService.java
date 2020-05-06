9d3785cabf2456317254c4c6838d54d8fadab95c
Manjit8200 <44630083+Manjit8200@users.noreply.github.com>
2020-05-06 17:07:52 +0530

    Addressbook service interface method declare

--- com/bridgelabz/codeinclub/services/AddressBookService.java
+++ com/bridgelabz/codeinclub/services/AddressBookService.java
@@ -1,9 +1,13 @@
   package com.bridgelabz.codeinclub.services;
+/*
+  Use Services and method declare only
+  @author manjit Khurana
+*/
 
 
   public interface AddressBookService
    {
-	void addPerson();
-	void showData();
-	void saveInfo();
+	void addPerson();     // calling Method to addPerson
+	void showData();     // calling Method Display Addressbook Person
+	void saveInfo();    // call to store data in csv 
    }

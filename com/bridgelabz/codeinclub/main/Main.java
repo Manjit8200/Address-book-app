dcf53dff9710cc3a6bee905cd4bb1e41bc343af6
Manjit8200 <44630083+Manjit8200@users.noreply.github.com>
2020-05-06 17:13:42 +0530

    Updatemain File With Services Implement

--- com/bridgelabz/codeinclub/main/Main.java
+++ com/bridgelabz/codeinclub/main/Main.java
@@ -4,52 +4,46 @@ Main AddressBook File
 @author Manjit Khurana
 */
 import com.bridgelabz.codeinclub.utils.ReadCSV;
-import java.util.*;
-
 import com.bridgelabz.codeinclub.utils.AddressBookUtill;
-
 import com.bridgelabz.codeinclub.services.AddressBookService;
-
 import com.bridgelabz.codeinclub.services.AddressBookServiceImpl;
+import java.util.*;
 
 
 public class Main{
     
-
+//Main Method
     public static void main(String[] args){
     AddressBookService addressBookService = new AddressBookServiceImpl(new ReadCSV());
    
-    // int select=0;
-
-		//while(select!=4){
-while(true){
+  	while(true){
         
-       System.out.println("Select the task to perform \n"+
+      		 System.out.println("Select the task to perform \n"+
 								"1. Create new People Entry \n"+
-								"2. Show existing Entry\n"+
-								"3. Save AddressBook \n"+
-								"4.Close AddressBook\n");     
-         final int select = AddressBookUtill.getUserNumber();
+								"2. Show Address-Book Entry\n"+
+								"3. Save Address-Book \n"+
+								"4.Close Address-Book\n");     
+         	final int select = AddressBookUtill.getUserNumber();
             	
-            switch(select){
+            	switch(select){
                 case 1:
-					 System.out.println("Add People Entry");
-					 addressBookService.addPerson();
-                break;
+		 System.out.println("Add People Entry");
+		 addressBookService.addPerson();
+                 break;
                 case 2:
-                    System.out.println("show Contacts Details");
-                    addressBookService.showData();
-                break;
-               
+                  System.out.println("show Contacts Details");
+                  addressBookService.showData();
+                  break;
                 case 3:
-                    System.out.println("Write in csv file ");
+                    System.out.println("Write data in csv file ");
                     addressBookService.saveInfo();
                     break;
-				case 4:
-				 System.out.println("Close AddressBook App");
-                System.exit(0);
-                default:
-                    System.out.println("Invalid range ");
+		case 4:
+		 System.out.println("Close Address-Book App");
+                 System.exit(0);
+                
+		default:
+                 System.out.println("Invalid range ");
             }
         }
 

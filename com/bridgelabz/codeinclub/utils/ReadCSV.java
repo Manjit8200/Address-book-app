061e749cb51ed739bdb0d4597b6c342a61e8dca8
Manjit8200 <44630083+Manjit8200@users.noreply.github.com>
2020-05-06 17:05:44 +0530

    Read data from UserInput

--- com/bridgelabz/codeinclub/utils/ReadCSV.java
+++ com/bridgelabz/codeinclub/utils/ReadCSV.java
@@ -13,7 +13,7 @@ public class ReadCSV{
 
      final ArrayList<People> peopleEntity = new ArrayList<>();
     
-
+// add to data in address book
      public void addToAddressBook(final People people){
         peopleEntity.add(people);
     }
@@ -23,16 +23,17 @@ public class ReadCSV{
     public void storeInfo(){
         WriteCSV.storeDetails(peopleEntity);
     }
+	
   // showDetails People-Entry Method  
    public void showDetails(){
         for(People ppl : peopleEntity){
             System.out.println("FirstName: "+ppl.getFirstName());
-			System.out.println("LastName: "+ppl.getLastName());
+	    System.out.println("LastName: "+ppl.getLastName());
             System.out.println("Address: "+ppl.getAddress());
             System.out.println("City: "+ppl.getCity());
-			System.out.println("State: "+ppl.getState());
+	    System.out.println("State: "+ppl.getState());
             System.out.println("Zip: "+ppl.getZip());
-		    System.out.println("PhoneNumber: "+ppl.getPhoneNumber());
+	    System.out.println("PhoneNumber: "+ppl.getPhoneNumber());
         }
     }
 }
